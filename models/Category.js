95f6aceb6c77f2b851a42356dedaf8748765a50b
Joey Thao <thaojoey5@gmail.com>
2023-11-10 14:21:56 -0600

    updated category.js

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Category extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class Category extends Model {}
 
 Category.init( {
     id: {
@@ -16,17 +11,11 @@ Category.init( {
         autoIncrement: true,
     },
     catergory_id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false, 
     },
   },
     {
-      hooks: {
-        beforeCreate: async (newUserData) => {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
-        },
-      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
