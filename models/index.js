83369f42acf4b3499651b9b936831097d33c5db5
Joey Thao <thaojoey5@gmail.com>
2023-11-11 13:33:02 -0600

    updated index.js

--- models/index.js
+++ models/index.js
@@ -3,50 +3,22 @@ const Product = require("./Product");
 const Category = require("./Category");
 const Photo = require("./Photo");
 
-// category have many products and products belong to category
-// product have many photo and photo belong to product
-
-// const ProductTag = require('./ProductTag');
-
-// Categories have many Products
 Category.hasMany(Product, {
     foreignKey: 'category_id',
     onDelete: 'SET NULL'
 });
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
     foreignKey: 'category_id',
 });
 
-// Products have many Photos
 Product.hasMany(Photo, {
     foreignKey: 'product_id',
     onDelete: 'CASCADE'
 });
 
-// Photo belong to Product
 Photo.belongsTo(Product, {
     foreignKey: 'product_id',
 });
-
-
-// Products belongToMany Tags (through ProductTag)
-// Product.belongsToMany(Tag, {
-//     through:{
-//         model: ProductTag,
-//         unique: false
-//     },
-//     as:"tags"
-// });
-
-// Tags belongToMany Products (through ProductTag)
-// Tag.belongsToMany(Product, {
-//     through:{
-//         model: ProductTag,
-//         unique: false
-//     },
-//     as:"products"
-// });
 
 module.exports = { User, Product, Category, Photo };
