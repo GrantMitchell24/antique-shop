93ff623be6f938a70a720d64177f3975d3b159a8
nestibry <nestibry@gmail.com>
2023-11-11 14:13:44 -0600

    changed key from category_name to title within the Category model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Product = require("./Product");
 const Category = require("./Category");
+const Product = require("./Product");
 const Photo = require("./Photo");
 
 Category.hasMany(Product, {
