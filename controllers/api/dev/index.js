7594e5800db7ace6907aa7f3201f818e2787176e
unknown <yacobabr@gmail.com>
2023-11-13 13:38:15 -0600

    api routes photo dev

--- controllers/api/dev/index.js
+++ controllers/api/dev/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const devUserRoutes = require('./devUserRoutes');
 const devCategoryRoutes = require('./devCategoryRoutes');
 const devProductRoutes = require('./devProductRoutes');
+const devPhotoRoutes = require('./devPhotoRoutes');
 
 router.use('/users', devUserRoutes);
 router.use('/categories', devCategoryRoutes);
 router.use('/products', devProductRoutes);
+router.use('/photos', devPhotoRoutes);
 
 module.exports = router;
