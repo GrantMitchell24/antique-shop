e7de9f8427a40a86acc35ce0b5d950727f1026ec
nestibry <nestibry@gmail.com>
2023-11-15 06:24:34 -0600

    new home route added logged_in to the serialize to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,6 +7,7 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
 
     try {
+        // Get all RECORDS and JOIN with other data
         const data = await Product.findAll({
             attributes: ['title', 'description','price'],
             include: [
@@ -14,18 +15,24 @@ router.get('/', async (req, res) => {
                 { model: Photo, attributes: ['url_link']}
             ]
         });
+
+        // Serialize data so the template can read it
         const products = data.map((item) => item.get({ plain: true }));
         const products2 = products.map(product => ({
             ...product,
             url_link: product.photos[0].url_link
         }));
-        res.render('homepage', {products: products2});
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            products: products2,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
 
 });
-
 
 
 
--- controllers/old/home-routes.js
+++ controllers/old/home-routes.js
@@ -6,6 +6,7 @@ const { User, Category, Product,  Photo } = require('../../models');
 router.get('/', async (req, res) => {
 
     try {
+        // Get all RECORDS and JOIN with other data
         const data = await Product.findAll({
             attributes: ['title', 'description','price'],
             include: [
@@ -13,12 +14,19 @@ router.get('/', async (req, res) => {
                 { model: Photo, attributes: ['url_link']}
             ]
         });
+
+        // Serialize data so the template can read it
         const products = data.map((item) => item.get({ plain: true }));
         const products2 = products.map(product => ({
             ...product,
             url_link: product.photos[0].url_link
         }));
-        res.render('homepage', {products: products2});
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            products: products2,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
