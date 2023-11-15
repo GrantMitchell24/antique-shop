85f94bf3225bc2bbbd2a496c2136e2cd47d0a6c8
nestibry <nestibry@gmail.com>
2023-11-14 20:33:29 -0600

    got it back to where it successfully logs in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,8 +25,8 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
-      res.render('main', {logged_in: req.session.logged_in});
-    //   res.json({ user: userData, message: 'You are now logged in!' });
+    //   res.render('main', {logged_in: req.session.logged_in});
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
