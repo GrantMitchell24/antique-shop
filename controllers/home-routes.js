f117648c75732a260b7c59b696a5587c2115d2aa
nestibry <nestibry@gmail.com>
2023-11-15 11:59:20 -0600

    cleaned up comments in home-routes adn product-page...verified still works as expected

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,28 +8,6 @@ router.get('/', async (req, res) => {
 
     try {
 
-        // // Get all RECORDS and JOIN with other data
-        // const data = await Product.findAll({
-        //     attributes: ['title', 'description', 'price'],
-        //     include: [
-        //         { model: Category, attributes: ['title'] },
-        //         { model: Photo, attributes: ['url_link'] }
-        //     ]
-        // });
-
-        // // Serialize data so the template can read it
-        // const products = data.map((item) => item.get({ plain: true }));
-        // const products2 = products.map(product => ({
-        //     ...product,
-        //     url_link: product.photos[0].url_link
-        // }));
-
-        // // Pass serialized data and session flag into template
-        // res.render('homepage', {
-        //     products: products2,
-        //     logged_in: req.session.logged_in
-        // });
-
         // Find all records and include other model data
         const data = await Product.findAll({
             attributes: ['id', 'title', 'description', 'price'],
@@ -47,7 +25,6 @@ router.get('/', async (req, res) => {
         }));
 
         // Pass serialized data and session flag into template
-        // res.status(200).json(products);
         res.render('homepage', {
             products: products,
             logged_in: req.session.logged_in
@@ -83,7 +60,6 @@ router.get('/product/:id', async (req, res) => {
         const product = {...serialData, url_link: serialData.photos[0].url_link};
         
         // Pass serialized data and session flag into template
-        // res.status(200).json(product);
         res.render('product-page', {
             ...product,
             logged_in: req.session.logged_in
