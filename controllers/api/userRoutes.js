8c1dc2d6794aee3a44c0129d67def1a6965f7d6b
nestibry <nestibry@gmail.com>
2023-11-14 18:53:52 -0600

    render homepage for login and init a logout link

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -25,7 +25,8 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res.render('homepage');
+    //   res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
