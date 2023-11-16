8c47731a04adf7a4ade5db0b4b226b01ddc3f2c6
nestibry <nestibry@gmail.com>
2023-11-15 21:43:01 -0600

    post api/user/invoice route sends an email invoice to a hardcode from/to recipients

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -63,17 +66,57 @@ router.post('/cart', async (req, res) => {
 
         console.log(req.body.id);
         req.session.save(() => {
-            if(!req.session.cart){
+            if (!req.session.cart) {
                 req.session.cart = req.body.id;
             } else {
                 // If doesn't already exist in cart, add the product
-                if(!req.session.cart.includes(req.body.id)){
+                if (!req.session.cart.includes(req.body.id)) {
                     req.session.cart = [...req.session?.cart, req.body.id];
                 }
             }
-            
-            res.status(200).json({status: "success", message: req.session.cart});
+
+            res.status(200).json({ status: "success", message: req.session.cart });
         });
+
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/invoice', async (req, res) => {
+    try {
+
+        console.log(req.body);
+
+
+        // Create a transporter with your Gmail account
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'nestinbk@gmail.com',
+                pass: process.env.EMAIL_APP_PASSWORD,
+            },
+        });
+
+
+        const mailOptions = {
+            from: 'nestinbk@gmail.com',
+            to: 'nestibry@gmail.com',
+            subject: 'Hello from Nodemailer! - Emailed Invoice!',
+            text: 'This is a test email sent using Nodemailer. Using an environment variable',
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+
+        res.status(200).json({ status: "success", message: req.body });
 
 
     } catch (err) {
